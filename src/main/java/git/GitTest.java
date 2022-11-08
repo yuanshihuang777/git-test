1cd9d0087f41ca47cd22b7bf77fdfd8921c7eb29
ysh <ysh@qq.com>
2022-11-08 19:19:23 +0800

    second commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -5,5 +5,6 @@ import sun.applet.Main;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2");
     }
 }
